81d97618f21ce34e227b5ee21828191793bba324
DrLeurs <nathankerstens@kreastion.be>
2023-03-06 22:46:08 +0100

    Create model for product

---
+++ backend/kreastion-api/models/product.js
@@ -0,0 +1,30 @@
+/**
+ * Model for product
+ */
+
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    reference: {
+        required: true,
+        type: String
+    },
+    name: {
+        required: true,
+        type: String
+    },
+    description: {
+        required: false,
+        type: String
+    },
+    composer: {
+        required: false,
+        type: String
+    },
+    price: {
+        required: true,
+        type: Number
+    }
+});
+
+module.exports = mongoose.model('Product', dataSchema);
