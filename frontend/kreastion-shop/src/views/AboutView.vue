fad7fb5a0c8100127bff5c6a44a8fbfa4fbff28c
DrLeurs <nathankerstens@kreastion.be>
2023-03-10 23:32:37 +0100

    main element styling in main.css

--- frontend/kreastion-shop/src/views/AboutView.vue
+++ frontend/kreastion-shop/src/views/AboutView.vue
@@ -10,13 +10,6 @@ export default {}
 
 <style scoped>
 @media (min-width: 1024px) {
-  main {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    color: red;
-  }
-
   h1 {
     color: var(--color-title);
   }
--- frontend/kreastion-shop/src/views/HomeView.vue
+++ frontend/kreastion-shop/src/views/HomeView.vue
@@ -4,19 +4,12 @@ export default {}
 
 <template>
   <main>
-    <h1>Kreastion Shop &mdash;</h1>
+    <h1>Kreastion Shop</h1>
   </main>
 </template>
 
 <style scoped>
 @media (min-width: 1024px) {
-  main {
-    margin-top: 2rem;
-    min-height: 100vh;
-    display: flex;
-    /* align-items: center; */
-  }
-
   h1 {
     color: var(--color-title);
   }
