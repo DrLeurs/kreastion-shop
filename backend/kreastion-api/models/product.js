43e64736a645800f039765674e8cd0a8e8974f85
DrLeurs <nathankerstens@kreastion.be>
2023-03-13 23:19:35 +0100

    Add instrumentation field to product model

--- backend/kreastion-api/models/product.js
+++ backend/kreastion-api/models/product.js
@@ -21,6 +21,10 @@ const dataSchema = new mongoose.Schema({
         required: false,
         type: String
     },
+    instrumentation: {
+        required: false,
+        type: String
+    },
     price: {
         required: true,
         type: Number
