662508e5c05ec341e7c5903ec93772c4e485651a
DrLeurs <nathankerstens@kreastion.be>
2023-03-15 02:37:55 +0100

    Fix: make cartstore totalQty getter

--- frontend/kreastion-shop/src/stores/CartStore.js
+++ frontend/kreastion-shop/src/stores/CartStore.js
@@ -3,7 +3,6 @@ import { defineStore } from 'pinia'
 export const useCartStore = defineStore('cart', {
     state: () => {
         return {
-            totalQty: 0,     // dummy/test
             cart: []
         }
     },
@@ -15,8 +14,6 @@ export const useCartStore = defineStore('cart', {
                 product: product,
                 qty: qty
             });
-            // increase total quantity of products
-            this.totalQty += qty;
         }
     },
 
@@ -25,6 +22,14 @@ export const useCartStore = defineStore('cart', {
             let total = 0;
             for(const orderLine of state.cart) {
                 total += orderLine.product.price * orderLine.qty;
+            }
+
+            return total;
+        },
+        totalQty: (state) => {
+            let total = 0;
+            for(const orderLine of state.cart) {
+                total += orderLine.qty;
             }
 
             return total;
